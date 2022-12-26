a6361f1aa39cfea61e63503c98858e2d9720c1c0
alejandrojosue <josued.ad13@gmail.com>
2022-12-25 20:59:04 -0600

    port modified

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const { dbConnect } = require('../database/config');
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usersPath = '/api/users';
 
         // Conectar a la base de datos
