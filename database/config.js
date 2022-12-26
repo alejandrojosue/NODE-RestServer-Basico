77999d448b452c2528c7d1ef2ce508372ad9e828
Alejandro Díaz <67884952+alejandrojosue@users.noreply.github.com>
2022-12-25 21:39:22 -0600

    Update config.js

--- database/config.js
+++ database/config.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 const dbConnect = async() => {
     try {
-        await mongoose.connect(process.env.MONGODB_CNN,  {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
+        await mongoose.connect(process.env.MONGODB_CNN);
         console.log('Base de datos conectada');
     } catch (err) {
         console.log(err);
